7d17a6c600980c908ce92a25b4d4a65cd3adf633
Ziyu <theorhythm@gmail.com>
2025-02-17 11:47:45 +0800

    12.2 敌方类-动画状态切换完成

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -1,17 +1,31 @@
 #include "enemy.h"
-#include "affiliate/sprite_anim.h"
+
 
 
 void Enemy::init()
 {
     Actor::init();
-    SpriteAnim::addSpriteAnimChild(this, "assets/sprite/ghost-Sheet.png", 2.0f);
+    anim_normal_ = SpriteAnim::addSpriteAnimChild(this, "assets/sprite/ghost-Sheet.png", 2.0f);
+    anim_hurt_ = SpriteAnim::addSpriteAnimChild(this, "assets/sprite/ghostHurt-Sheet.png", 2.0f);
+    anim_die_ = SpriteAnim::addSpriteAnimChild(this, "assets/sprite/ghostDead-Sheet.png", 2.0f);
+    anim_hurt_->setActive(false);
+    anim_die_->setActive(false);
+
+    current_anim_ = anim_normal_;
+
 }
 
 void Enemy::update(float dt){
     Actor::update(dt);
     aim_target(target_);
     move(dt);
+    timer_ += dt;
+    if (timer_ > 2.0f && timer_ < 4.0f) {
+        changeState(State::HURT);
+    } else if (timer_ > 4.0f) {
+        changeState(State::DIE);
+    }
+    
 }
 
 
@@ -21,4 +35,29 @@ void Enemy::aim_target(Player *target)
     auto direction = target->getPosition() - this->getPosition();
     direction = glm::normalize(direction);
     velocity_ = direction * max_speed_;
+}
+
+void Enemy::checkState()
+{
+}
+
+void Enemy::changeState(State new_state)
+{
+    if (new_state == current_state_) return;
+    current_anim_->setActive(false);
+    switch (new_state) {
+    case State::NORMAL:
+        current_anim_ = anim_normal_;
+        current_anim_->setActive(true);
+        break;
+    case State::HURT:
+        current_anim_ = anim_hurt_;
+        current_anim_->setActive(true);
+        break;
+    case State::DIE:
+        current_anim_ = anim_die_;
+        current_anim_->setActive(true);
+        break;
+    }
+    current_state_ = new_state;
 }
