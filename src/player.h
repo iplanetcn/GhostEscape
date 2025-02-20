5aff74645f2ecff47f04cd0ec3da8112beb05fec
Ziyu <theorhythm@gmail.com>
2025-02-20 17:11:05 +0800

    20 武器类完成

--- src/player.h
+++ src/player.h
@@ -4,9 +4,11 @@
 #include "core/actor.h"
 #include "affiliate/sprite_anim.h"
 #include "world/effect.h"
+#include "weapon_thunder.h"
 
 class Player : public Actor
 {
+    WeaponThunder* weapon_thunder_ = nullptr;
     SpriteAnim* sprite_idle_ = nullptr;
     SpriteAnim* sprite_move_ = nullptr;
     Effect* effect_ = nullptr;
---
+++ src/weapon_thunder.h
@@ -0,0 +1,16 @@
+#ifndef WEAPON_THUNDER_H
+#define WEAPON_THUNDER_H
+
+#include "raw/weapon.h"
+
+class WeaponThunder : public Weapon
+{
+public:
+    static WeaponThunder* addWeaponThunderChild(Actor* parent, float cool_down, float mana_cost);
+
+    virtual void handleEvents(SDL_Event& event) override;
+};
+
+
+
+#endif // WEAPON_THUNDER_H
